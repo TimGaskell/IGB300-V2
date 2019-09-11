173fa0d197c81fce646706604282d8d3aa30116d
Tim Gaskell <tgaskell23@gmail.com>
2019-09-11 20:24:36 +1000

    Basic Surge fixing
    it was fine before

--- Space Traitors Host/Assets/Scripts/Server/Server.cs
+++ Space Traitors Host/Assets/Scripts/Server/Server.cs
@@ -2237,7 +2237,6 @@ public class Server : MonoBehaviour
                     case (GameManager.TurnPhases.BasicSurge):
                         PlayerCardManager.instance.UpdateAllCards();
                         GameManager.instance.IncrementTurn();
-                        SendSurge();
                         break;
                     case (GameManager.TurnPhases.AttackSurge):
                         //Need to display surge information on main screen
