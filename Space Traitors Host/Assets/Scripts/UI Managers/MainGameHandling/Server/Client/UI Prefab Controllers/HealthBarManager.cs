da77aa9d89d7809a316eaca9f70e8e537c21bee3
Joshua <joshuaburton2@gmail.com>
2019-09-18 21:27:38 +1000

    Health Point Bar Working
    Health Point Bar working as intended

--- Space Traitors Host/Assets/Scripts/UI Managers/MainGameHandling/Server/Client/UI Prefab Controllers/HealthBarManager.cs
+++ Space Traitors Host/Assets/Scripts/UI Managers/MainGameHandling/Server/Client/UI Prefab Controllers/HealthBarManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarManager : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class HealthBarManager : MonoBehaviour
 
     private const int backgroundIncrement = 60;
 
-    private void Start()
+    private void Awake()
     {
         healthPoints = new List<GameObject>();
 
@@ -21,17 +22,23 @@ public class HealthBarManager : MonoBehaviour
     public void UpdateHealthPoints()
     {
         //Add new health points if the UI has less than the max number of hit points represented
+        RectTransform backgroundRectTrans = pointBackground.GetComponent<RectTransform>();
+
         while (healthPoints.Count < ClientManager.instance.maxLifePoints)
         {
             healthPoints.Add(Instantiate(initialPoint, pointBackground.transform));
-        } 
 
+            float currentWidth = backgroundRectTrans.sizeDelta.x;
+            float currentHeight = backgroundRectTrans.sizeDelta.y;
+
+            pointBackground.GetComponent<RectTransform>().sizeDelta = new Vector2(currentWidth + backgroundIncrement, currentHeight);
+        }
+
+        //
         for (int healthIndex = 0; healthIndex < ClientManager.instance.maxLifePoints; healthIndex++)
         {
-            if(healthIndex < ClientManager.instance.lifePoints)
-            {
-                healthPoints[healthIndex].transform.GetChild(0).gameObject.SetActive(true);
-            }
+            GameObject pointMarker = healthPoints[healthIndex].transform.GetChild(0).gameObject;
+            pointMarker.SetActive(healthIndex < ClientManager.instance.lifePoints);
         }
     }
 }
