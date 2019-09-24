e446e66c745b58d689829658703b9410c3ac9ceb
Joshua <joshuaburton2@gmail.com>
2019-09-24 17:47:50 +1000

    Client Main Menu Update

--- Space Traitors Host/Assets/Scripts/UI Managers/MainMenuHandling/ClientMenuUIManager.cs
+++ Space Traitors Host/Assets/Scripts/UI Managers/MainMenuHandling/ClientMenuUIManager.cs
@@ -6,6 +6,15 @@ using UnityEngine.UI;
 
 public class ClientMenuUIManager : MonoBehaviour
 {
+    private const int PANELS_CLOSED = -1;
+
+    public List<GameObject> instructionPanels;
+    public int currentPanel;
+
+    private void Start()
+    {
+        currentPanel = PANELS_CLOSED;
+    }
 
     public void GameStart()
     {
@@ -15,5 +24,18 @@ public class ClientMenuUIManager : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenNextInstructionPanel()
+    {
+        instructionPanels[currentPanel].SetActive(false);
+        currentPanel++;
+        instructionPanels[currentPanel].SetActive(true);
+    }
+
+    public void CloseInstructionPanels()
+    {
+        instructionPanels[currentPanel].SetActive(false);
+        currentPanel = PANELS_CLOSED;
     }
 }
