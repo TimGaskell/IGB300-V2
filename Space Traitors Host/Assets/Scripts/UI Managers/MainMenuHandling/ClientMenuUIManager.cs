f46c3b39f6bb6b3343bb34064480ba27b08784f1
Joshua <joshuaburton2@gmail.com>
2019-09-24 22:54:55 +1000

    Client Instruction Screen
    Added instruction panels to client main menu

--- Space Traitors Host/Assets/Scripts/UI Managers/MainMenuHandling/ClientMenuUIManager.cs
+++ Space Traitors Host/Assets/Scripts/UI Managers/MainMenuHandling/ClientMenuUIManager.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class ClientMenuUIManager : MonoBehaviour
 {
     private const int PANELS_CLOSED = -1;
 
     public List<GameObject> instructionPanels;
+    public GameObject parentInstructionPanel;
+    public GameObject instructionCounter;
     public int currentPanel;
 
     private void Start()
     {
         currentPanel = PANELS_CLOSED;
+        parentInstructionPanel.SetActive(false);
+
+        foreach (GameObject instructionPanel in instructionPanels)
+        {
+            instructionPanel.SetActive(false);
+        }
     }
 
     public void GameStart()
@@ -28,14 +37,32 @@ public class ClientMenuUIManager : MonoBehaviour
 
     public void OpenNextInstructionPanel()
     {
-        instructionPanels[currentPanel].SetActive(false);
-        currentPanel++;
-        instructionPanels[currentPanel].SetActive(true);
+        if (currentPanel == instructionPanels.Count - 1)
+        {
+            CloseInstructionPanels();
+        }
+        else
+        {
+            if (currentPanel == PANELS_CLOSED)
+            {
+                parentInstructionPanel.SetActive(true);
+            }
+
+            else
+            {
+                instructionPanels[currentPanel].SetActive(false);
+            }
+
+            currentPanel++;
+            instructionCounter.GetComponent<TextMeshProUGUI>().text = string.Format("{0} / {1}", currentPanel + 1, instructionPanels.Count);
+            instructionPanels[currentPanel].SetActive(true);
+        }
     }
 
     public void CloseInstructionPanels()
     {
         instructionPanels[currentPanel].SetActive(false);
+        parentInstructionPanel.SetActive(false);
         currentPanel = PANELS_CLOSED;
     }
 }
