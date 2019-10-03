2975eeea58035c307e205613e59efe02411cf203
Joshua <joshuaburton2@gmail.com>
2019-10-03 20:30:14 +1000

    Name Entry Update
    Added a character cap to name entry and an error text display.

--- Space Traitors Host/Assets/Scripts/UI Managers/LobbyHandling/ClientUiManager.cs
+++ Space Traitors Host/Assets/Scripts/UI Managers/LobbyHandling/ClientUiManager.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class ClientUiManager : MonoBehaviour
 {
     public GameObject PlayerInput;
+    public GameObject errorText;
+    private const int maxChar = 25;
 
     public void Start() {
 
-      
+        SetErrorText("", Color.black);
 
     }
 
 
-    public void SubmitPlayerName(GameObject TextField) {
+    public void SubmitPlayerName(GameObject TextField)
+    {
 
         string name = TextField.GetComponent<TMP_InputField>().text;
 
-        if (name != "") {
+        if (name != "")
+        {
+            if (name.Length > maxChar)
+            {
+                SetErrorText("Name is too long", Color.red);
+                TextField.GetComponent<TMP_InputField>().text = "";
+            }
+            else
+            {
+                PlayerInput.GetComponent<CanvasGroup>().interactable = false;
+                SetErrorText("Please wait", Color.white);
+                Server.Instance.SendPlayerInformation(name);
+                ClientManager.instance.playerName = name;
+            }
+        }
+        
+        else
+        {
+            SetErrorText("Please enter a name", Color.red);
+        }
+    }
 
-            PlayerInput.GetComponent<CanvasGroup>().interactable = false;
-            Server.Instance.SendPlayerInformation(name);
-            ClientManager.instance.playerName = name;
-           
-            
-        }
-        else {
-            Debug.Log("Enter a name");
-        }
+    private void SetErrorText(string errorString, Color fontColor)
+    {
+        errorText.GetComponent<TextMeshProUGUI>().text = errorString;
+        errorText.GetComponent<TextMeshProUGUI>().color = fontColor;
     }
 }
