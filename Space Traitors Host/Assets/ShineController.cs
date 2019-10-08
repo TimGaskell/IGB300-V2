f7d6786a7cd44e8980e15d91d46d846b890829d8
Tom <tks.hawkins11@gmail.com>
2019-10-08 18:54:27 +1000

    UI Changes

---
+++ Space Traitors Host/Assets/ShineController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ShineController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> Shines = new List<GameObject>();
+    [FormerlySerializedAs("WaitTime")] public float maxWaitTime = 1f;
+    public float minWaitTime = 1f;
+    private static readonly int Shine1 = Animator.StringToHash("Shine");
+
+
+    void Start()
+    {
+        StartCoroutine(Shine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    IEnumerator Shine()
+    {
+        while (true)
+        {
+            foreach (var shine in Shines)
+            {
+                if (shine != null) shine.GetComponent<Animator>().SetTrigger(Shine1);
+                yield return new WaitForSeconds(Random.Range(min: minWaitTime, max: maxWaitTime));
+            }
+        }
+        
+    }
+
+    
+}
+
+
